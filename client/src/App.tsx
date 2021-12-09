5edc1e677ad77541f467324dc81a2e339b3f151d
Michael Jacobs <jacobsm84@gmail.com>
2021-12-08 19:09:39 -0500

    Landing page created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,18 @@
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Landing from './pages/landing/landing';
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
